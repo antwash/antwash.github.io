d31bca24da4f52f309e4aaec37b4f42759af12b2
Anthony Washington <awashington251@gmail.com>
2019-03-02 21:03:38 -0800

    Add navigation header and title
    - Adds the folder structure
    - Adds the navigation bar
    - Adds the navigation bar titles
    
    TO-DO: Add line when item is selected

---
+++ src/components/navigation/NavigationBar.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'; 
+import { Nav, Navbar } from 'react-bootstrap';
+import NavigationBarItem from './NavigationItem';
+
+class NavigationBar extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      navItems: [
+        {title: 'home', selected: false},
+        {title: 'about', selected: false},
+        {title: 'contact', selected: false}
+      ],
+    }
+  }
+
+  render() {
+    var list_items = [];
+    const navItems = this.state.navItems;
+  
+    for (var index = 0; index < navItems.length; index++) {
+
+      const pathName = window.location.pathname.slice(1);
+      const navBarItemTitle = navItems[index].title;
+      const navBarItemSelected = (navBarItemTitle === pathName ? true:false);
+      
+      list_items.push(
+        <NavigationBarItem 
+          key={index}
+          title={navBarItemTitle}
+          selected={navBarItemSelected}
+          />
+      )
+    }
+
+    return(
+        <Navbar className="nav-bar">
+          <Navbar.Brand href="#home">
+            <div className="brand-initial" > A </div>
+          </Navbar.Brand>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              {list_items}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+    )
+  }
+}
+
+export default NavigationBar;
